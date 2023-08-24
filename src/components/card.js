78c6ecbf1001ad4d0acbb1cf9b4385c4b7d561d3
parapsychic <febinkdominic@outlook.com>
2023-08-25 00:12:17 +0530

    Fixed an issue with WebKit based browsers

--- src/components/card.js
+++ src/components/card.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Card = ({project:{title, imgUrl, desc}, onClick}) => {
   return (<Link to={onClick} target="_blank" rel="noopener noreferrer">
     <div className='rounded-lg bg-stroke-blue border-none transiton ease-in-out delay-100 hover:drop-shadow-2xl duration-300 overflow-hidden max-w-xs h-fit '>
-    <img className='h-fit object-cover' src={imgUrl} alt={title}/>
+    <img src={imgUrl} alt={title}/>
     <div className='px-3'>
 
     <p className='text-xl mt-4 mb-2'>{title}</p>
